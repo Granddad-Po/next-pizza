0ee634aee816efe1144a599c8c61983834609472
Vitalii Potapov <granddad.po@gmail.com>
2025-01-14 11:55:24 +0530

    add: Header and Title components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { Container, Title } from "@/components/shared";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+    </>
+  );
 }
